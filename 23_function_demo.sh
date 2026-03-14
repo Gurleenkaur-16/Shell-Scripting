990931db6461ce535d73ebbd242ad9302fc50f29
Ubuntu <ubuntu@ip-172-31-32-114.eu-west-1.compute.internal>
2026-03-14 18:59:59 +0000

    shell scripting

--- 23_function_demo.sh
+++ 23_function_demo.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+function myfun {
+echo "This is my first function "
+}
+hi(){
+echo "hello"
+}
+# To call the function
+myfun
+hi
 
--- 24_function_with_arg.sh
+++ 24_function_with_arg.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+#function with arguments
+
+addition() {
+num1=$1
+num2=$2
+let sum=$num1+$num2
+echo " sum of $num1 & $num2 is $sum"
+}
+addition 12 23 
 
