564bb3bf6492d27a226a9ba02799d78451aa9279
Ubuntu <ubuntu@ip-172-31-32-114.eu-west-1.compute.internal>
2026-03-14 17:18:48 +0000

    added while loop

--- 22_while_with_csv.sh
+++ 22_while_with_csv.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+#reasd csv file data using while loop
+
+cat test.cv | awk 'NR!=1 {print}' |while IFS="," read id name age
+do
+echo "id is $id"
+echo "name is $name"
+echo "age is $age"
+done 
 
