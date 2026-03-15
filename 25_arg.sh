ece0d76c5dc612b48e494cb71485acafc500a462
Ubuntu <ubuntu@ip-172-31-32-114.eu-west-1.compute.internal>
2026-03-15 08:14:35 +0000

    shell script

--- 25_arg.sh
+++ 25_arg.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+#to access the arguments 
+
+echo "First argument is $1"
+echo "Seconfd argument is $2"
+
+echo "All the arguments are  - $@"
+echo "Number of arguments are - $#"
+
+#for loop to access value from arguments
+
+for filename in $@
+do
+echo " copying file - $filename"
+done
 
--- 26_shift_arg.sh
+++ 26_shift_arg.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+#Creating a user and there description 
+
+echo "user"
+echo "username is $1"
+echo "userid is $2"
+shift
+shift
+echo " Description is $@"
 
