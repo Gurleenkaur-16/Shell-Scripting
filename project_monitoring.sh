28fbc95c9f82f658e72e017edda1b1caf6309a1a
Ubuntu <ubuntu@ip-172-31-32-114.eu-west-1.compute.internal>
2026-03-19 12:43:06 +0000

    project

--- project_monitoring.sh
+++ project_monitoring.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+FREE_SPACE=$(free -mt | grep "Total" | awk '{print $4}')
+TH=300
+
+if [[ $FREE_SPACE -lt $TH ]]
+then
+echo "WARNING, RAM is running low"
+else
+echo "RAM SPACE is sufficient - $FREE_SPACE M"
+fi
 
