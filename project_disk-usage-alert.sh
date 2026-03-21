3a5556790cdd6990b0ca594d8fc20c91d176dddf
Ubuntu <ubuntu@ip-172-31-32-114.eu-west-1.compute.internal>
2026-03-21 14:02:17 +0000

    project

--- project_disk-usage-alert.sh
+++ project_disk-usage-alert.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+#monitoring free dish space 
+
+FU=$(df -H | egrep -v "tmpfs" | grep "xvda16" | awk '{print $5}' | tr -d %)
+
+if [[ $FU -ge 80 ]]
+then 
+echo " Warning disk space is low - $FU % "
+else
+echo " Enough space "
+fi
 
